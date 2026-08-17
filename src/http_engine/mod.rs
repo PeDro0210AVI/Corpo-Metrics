dc17c53c7ac6486155b884c2bc9dca35737673c8
PeDro0210 <pedroruben021@gmail.com>
2026-08-17 12:10:56 -0600

    chore: adding first implementations of http engine

--- src/http_engine/mod.rs
+++ src/http_engine/mod.rs
@@ -1,4 +1,31 @@
+use async_std::net::TcpListener;
+use futures::StreamExt;
+use std::format;
+
 struct HttpServer {
     host: String,
     port: usize,
+    listener: TcpListener,
+}
+
+impl HttpServer {
+    pub async fn new(host: String, port: usize) -> Self {
+        let listener = TcpListener::bind(format!("{host}:{port}"))
+            .await
+            .expect("Couldn't bind to address");
+
+        Self {
+            host,
+            port,
+            listener,
+        }
+    }
+
+    pub async fn run(self) {
+        self.listener
+            .incoming()
+            .for_each_concurrent(None, |tcpstream| async move {
+                //TODO: add simple handler
+            });
+    }
 }
---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod http_engine;
+pub mod server;
