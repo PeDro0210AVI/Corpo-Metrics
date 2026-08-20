a3ead382035406c578e7b979a52f396ef7803362
Pedro Avila <pedro021@Pedros-MacBook-Air.local>
2026-08-19 22:30:26 -0600

    chore: added primitives for call for mcp

--- src/server/primitives/mod.rs
+++ src/server/primitives/mod.rs
@@ -49,6 +49,11 @@ pub struct ContentText {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct McpToolListNames {
+    names: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct McpPayload {
     resultType: ResultType,
     content: Vec<ContentText>,
@@ -58,6 +63,7 @@ pub struct McpPayload {
 pub enum McpServerError {
     NotMethodSupply,
     CouldntFullFilledResponse,
+    CouldntGetCallArguments,
 }
 
 impl Display for McpServerError {
@@ -90,19 +96,54 @@ impl McpServer {
     }
 
     //TODO: all opening files things load them at startup
-    async fn mcp_call(json_rpc_request: McpJson) -> Result<McpPayload, Box<dyn Error>> {
-        // for getting al
-        let mut file = File::open("data/mcp/mcp_tools_list.json").await?;
+    async fn mcp_call(json_rpc_request: McpJson) -> Result<McpJson, Box<dyn Error>> {
+        if let None = json_rpc_request.params {
+            return Err(Box::new(McpServerError::CouldntFullFilledResponse));
+        };
 
-        let content = &mut String::new();
+        let mcp_params = json_rpc_request.params.unwrap();
 
-        file.read_to_string(content).await?;
+        let result: Result<McpPayload, McpServerError> = match mcp_params.name.as_str() {
+            "get_sales_metrics" => {
+                todo!()
+            }
+            "get_top_products" => {
+                todo!()
+            }
+            "get_financial_status" => {
+                todo!()
+            }
+            "get_customer_metrics" => {
+                todo!()
+            }
+            "get_project_status" => {
+                todo!()
+            }
+            "get_team_metrics" => {
+                todo!()
+            }
+            "get_product_health" => {
+                todo!()
+            }
+            "generate_executive_summary" => {
+                todo!()
+            }
+            "get_anomalies" => {
+                todo!()
+            }
+            "get_inventory_levels" => {
+                todo!()
+            }
+            _ => Err(McpServerError::CouldntGetCallArguments),
+        };
 
-        let mut deserializer = serde_json::Deserializer::from_str(&content);
-        let deserializer = serde_stacker::Deserializer::new(&mut deserializer);
-        //TOD: parse MCP name
+        let result = Some(serde_json::to_value(result?)?);
 
-        todo!()
+        Ok(McpJson {
+            jsonrpc: "2.0".to_string(),
+            result,
+            ..Default::default()
+        })
     }
 
     async fn mcp_list() -> Result<McpJson, Box<dyn Error>> {
